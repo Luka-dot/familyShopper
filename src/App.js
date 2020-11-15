460adcd0d240fe095fd6ae78ab55ab091d7b13f9
Luka-dot <79lukask@gmail.com>
2020-11-15 18:55:16 -0500

    connect to state and dispatch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-import { useSelector, connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { increment, decrement } from './redux/actions/index';
 import MainList from './components/main-list/main-list.component';
 import CustomButton from './components/button/custom-button-component';
@@ -7,19 +7,12 @@ import CustomButton from './components/button/custom-button-component';
 import './App.scss';
 
 function App(props) {
-  const counter = useSelector(state => state.counter)
-  //const dispatch = useDispatch();
-
-  const handlePlus = () => {
-    console.log("+", increment)
-    props.increment(3)
-  }
 
   return (
     <div className="App">
-      <h5>counter is {counter}</h5>
+      <h5>counter is {props.counter}</h5>
 
-      <button onClick={handlePlus}>+</button>
+      <button onClick={props.increment}>+</button>
       <button onClick={props.decrement}>-</button>
       <MainList />
       <CustomButton />
@@ -27,9 +20,13 @@ function App(props) {
   );
 }
 
+const mapStateToProps = state => ({
+  counter: state.counter
+});
+
 const mapDispatchToProps = dispatch => ({
-  increment: num => dispatch(increment(num)),
+  increment: num => dispatch(increment(3)),
   decrement: () => dispatch(decrement()),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
