4b4984aeebccdd8925f9c6fc2a71b90d2c7e5810
Luka-dot <79lukask@gmail.com>
2020-11-22 19:26:19 -0500

    user routs for sign up and log in created and tested

--- backEnd/app.js
+++ backEnd/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const directoryRoutes = require('./routes/directory-list-routes');
+const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(bodyParser.json())
 
 app.use('/api/directory/', directoryRoutes);
+app.use('/api/users', usersRoutes);
 
 // this middleware only runs after no route was found-matched
 app.use((req, res, next) => {
--- backEnd/controlers/users-controler.js
+++ backEnd/controlers/users-controler.js
@@ -1,4 +1,6 @@
-DUMMY_USERS = [
+const HttpError = require;
+
+const DUMMY_USERS = [
     {
         id: 1,
         nanme: "Lukas",
@@ -26,7 +28,16 @@ const signup = (req, res, next) => {
     res.status(201).json({user: createdUser});
 };
 
-const login = (req, res, next) => {};
+const login = (req, res, next) => {
+    const { email, password } = req.body;
+
+    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+    if (!identifiedUser || identifiedUser.password !== password) {
+        throw new HttpError('User not identified, not walid credentials.', 401);
+    }
+    
+    res.json({message: "User logged in."})
+};
 
 exports.getUsers = getUsers;
 exports.signup = signup;
